5529eddf72e5fd6674737b8d6a0882f9780bf071
shubhamgitvns <shubhammaurya10092002@gmail.com>
2024-04-26 12:08:47 +0530

    add logo in the image folder

--- lib/ui_page/home_page/dth/select_provider.dart
+++ lib/ui_page/home_page/dth/select_provider.dart
@@ -23,6 +23,45 @@ class _Select_ProviderState extends State<Select_Provider> {
           )
         ],
       ),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.grey.shade100,
+            child: const Text("Airtel Digital Tv"),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[200],
+            child: const Text('Heed not the rabble'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[300],
+            child: const Text('Sound of screams but the'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[400],
+            child: const Text('Who scream'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[500],
+            child: const Text('Revolution is coming...'),
+          ),
+          Container(
+            padding: const EdgeInsets.all(8),
+            color: Colors.teal[600],
+            child: const Text('Revolution, they...'),
+          ),
+        ],
+      ),
     );
   }
 }
