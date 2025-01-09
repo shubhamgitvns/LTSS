2ecb04da06fe1a5d9cd18df95a14f9063ae99db7
shubhamgitvns <shubhammaurya10092002@gmail.com>
2025-01-09 22:18:56 +0530

    update the welcome page

--- lib/ui_page/welcome_page.dart
+++ lib/ui_page/welcome_page.dart
@@ -5,89 +5,66 @@ import 'package:flutter/material.dart';
 import '../localdatabase.dart';
 import '../user_authentication/login_page.dart';
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String _connectionStatus = 'Unknown';
-  String connection = "";
-
-  @override
-  void initState() {
-    super.initState();
-
-    Timer(const Duration(seconds: 1), () async {
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 2), () async {
       print("enter the timer");
-      if (!mounted) return; // Check if widget is still mounted
       var list = await DatabaseHandler.jsons();
-      if (!mounted) return; // Check if widget is still mounted
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => Login()),
       );
-      // List<Json> lst = list;
-      // if (list[0].status == "success") {
-      //   print("enter the pin");
-      //   // App_Text.dbmobile = list[0].mobile;
-      //   // App_Text.dbfinger = list[0].finger;
-      //   Navigator.pushReplacement(
-      //     context,
-      //     MaterialPageRoute(builder: (context) => Pin()),
-      //   );
-      //   print("over the code");
-      //   // _authenticate();// Call _authenticate after navigation
-      // }
-
-      // if (list[0].status != "success") {
-      //   Navigator.pushReplacement(
-      //     context,
-      //     MaterialPageRoute(builder: (context) => Login()),
-      //   );
-      // }
     });
-  }
 
-  @override
-  void dispose() {
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          const SizedBox(
-            height: 200,
-          ),
-          const SizedBox(
-            height: 100,
-            child: Center(
-              child: Image(
-                image: AssetImage("images/lts.png"),
-              ),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          Widget content = SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const SizedBox(
+                  height: 200,
+                ),
+                const SizedBox(
+                  height: 100,
+                  child: Center(
+                    child: Image(
+                      image: AssetImage("images/lts.png"),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 200,
+                ),
+                Text(
+                  "Power by Sampurna",
+                  style: TextStyle(fontSize: 20, color: Colors.grey.shade400),
+                ),
+              ],
             ),
-          ),
-          const SizedBox(
-            height: 200,
-          ),
-          Text("Power by Sampurna",
-              style: TextStyle(fontSize: 20, color: Colors.grey.shade400))
-        ],
+          );
+
+          if (constraints.maxWidth > 600) {
+            // Larger screen size
+            return Container(
+              margin: EdgeInsets.all(16.0),
+              child: content,
+            );
+          } else {
+            // Smaller screen size
+            return Container(
+              margin: EdgeInsets.all(8.0),
+              child: content,
+            );
+          }
+        },
       ),
     );
   }
-}
-
-enum _SupportState {
-  unknown,
-  supported,
-  unsupported,
 }
